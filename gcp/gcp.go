5ea17238d8477f5bc9d2e4d329471ac2a61be74b
Kief H. Shemul <kiefhossain@pathao.com>
2020-11-28 13:17:32 +0600

    add gcp cloud dns updater

---
+++ gcp/gcp.go
@@ -0,0 +1,193 @@
+package gcp
+
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/api/dns/v1"
+	"reflect"
+	"strings"
+)
+
+type Client struct {
+	project    string
+	context    context.Context
+	dnsService *dns.Service
+}
+
+func New(project string) *Client {
+	ctx := context.Background()
+
+	// Require GOOGLE_APPLICATION_CREDENTIALS or pass plain credential files as byte in options
+	dnsService, err := dns.NewService(ctx)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
+	return &Client{
+		project:    project,
+		context:    ctx,
+		dnsService: dnsService,
+	}
+}
+
+func (this *Client) DnsRecordsByNameAndType(names []string, recordType string) (DnsRecords, error) {
+	records, err := this.DnsRecords()
+	if err != nil {
+		return nil, err
+	}
+	found := filterDnsRecordsByName(records, names)
+	found = filterDnsRecordsByType(found, recordType)
+	if len(found) != len(names) {
+		return nil, errors.New(fmt.Sprintf("Expected DNS records <%v> of type <%v>, but only found <%v> of them from the available <%v>",
+			strings.Join(names, ", "),
+			recordType,
+			strings.Join(found.NamesAndTypes(), ", "),
+			strings.Join(records.NamesAndTypes(), ", ")))
+	}
+	return found, nil
+}
+
+func filterDnsRecordsByName(records DnsRecords, names []string) DnsRecords {
+	if len(names) == 0 {
+		return DnsRecords{}
+	}
+	var results DnsRecords
+	for _, record := range records {
+		if equalsAny(record.Name, names) {
+			results = append(results, record)
+		}
+	}
+	return results
+}
+
+func equalsAny(haystack string, needles []string) bool {
+	for _, needle := range needles {
+		if haystack == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func filterDnsRecordsByType(records DnsRecords, recordType string) DnsRecords {
+	var results DnsRecords
+	for _, record := range records {
+		if record.Type == recordType {
+			results = append(results, record)
+		}
+	}
+	return results
+}
+
+func (this *Client) DnsRecords() (DnsRecords, error) {
+	zones, err := this.ManagedZones()
+	if err != nil {
+		return nil, err
+	}
+	var records DnsRecords
+	for _, zone := range zones {
+		rrsets, err := this.ResourceRecordSets(zone.Name)
+		if err != nil {
+			return nil, err
+		}
+		for _, rrset := range rrsets {
+			record := &DnsRecord{zone.Name, rrset}
+			records = append(records, record)
+		}
+
+	}
+	return records, nil
+}
+
+func (this *Client) ManagedZones() ([]*dns.ManagedZone, error) {
+	var results []*dns.ManagedZone
+	err := this.dnsService.ManagedZones.List(this.project).Pages(this.context, func(page *dns.ManagedZonesListResponse) error {
+		for _, zone := range page.ManagedZones {
+			results = append(results, zone)
+		}
+		return nil
+	})
+	return results, err
+}
+
+func (this *Client) ResourceRecordSets(managedZone string) ([]*dns.ResourceRecordSet, error) {
+	var results []*dns.ResourceRecordSet
+	req := this.dnsService.ResourceRecordSets.List(this.project, managedZone)
+	err := req.Pages(this.context, func(page *dns.ResourceRecordSetsListResponse) error {
+		for _, rrset := range page.Rrsets {
+			results = append(results, rrset)
+		}
+		return nil
+	})
+	return results, err
+}
+
+func (this *Client) UpdateDnsRecords(records DnsRecords, newValues []string) (DnsRecords, error) {
+	var updated DnsRecords
+	for managedZone, recordsInZone := range records.GroupByZone() {
+		changes := changesToUpdateDnsRecordValues(recordsInZone, newValues)
+		if changes == nil {
+			continue
+		}
+		resp, err := this.dnsService.Changes.Create(this.project, managedZone, changes).Context(this.context).Do()
+		if err != nil {
+			return nil, err
+		}
+
+		updated = append(updated, ToDnsRecords(managedZone, resp.Additions)...)
+	}
+	return updated, nil
+}
+
+func changesToUpdateDnsRecordValues(records DnsRecords, newValues []string) *dns.Change {
+	changes := &dns.Change{}
+	for _, record := range records {
+		if reflect.DeepEqual(record.Rrdatas, newValues) {
+			continue
+		}
+		changes.Deletions = append(changes.Deletions, record.ResourceRecordSet)
+		addition := *record.ResourceRecordSet
+		addition.Rrdatas = newValues
+		changes.Additions = append(changes.Additions, &addition)
+	}
+	if len(changes.Additions) == 0 {
+		return nil
+	}
+	return changes
+}
+
+type DnsRecord struct {
+	ManagedZone string
+	*dns.ResourceRecordSet
+}
+
+func (record DnsRecord) NameAndType() string {
+	return fmt.Sprintf("%v %v", record.Name, record.Type)
+}
+
+type DnsRecords []*DnsRecord
+
+func ToDnsRecords(managedZone string, records []*dns.ResourceRecordSet) DnsRecords {
+	var results DnsRecords
+	for _, record := range records {
+		results = append(results, &DnsRecord{ManagedZone: managedZone, ResourceRecordSet: record})
+	}
+	return results
+}
+
+func (records DnsRecords) GroupByZone() map[string]DnsRecords {
+	byZone := make(map[string]DnsRecords)
+	for _, record := range records {
+		byZone[record.ManagedZone] = append(byZone[record.ManagedZone], record)
+	}
+	return byZone
+}
+
+func (records DnsRecords) NamesAndTypes() []string {
+	names := make([]string, len(records))
+	for i, record := range records {
+		names[i] = record.NameAndType()
+	}
+	return names
+}
